584952add8e4a3fdcab996b2492750a537b6be30
Sahil <sahilkhan05861@gmail.com>
2026-07-14 12:58:58 +0530

    added new

--- attendance-backend/src/routes/test.routes.js
+++ attendance-backend/src/routes/test.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
+const State = require("../models/State");
+const City = require("../models/City");
 
 const auth =
-require("../middleware/auth.middleware");
+    require("../middleware/auth.middleware");
 
 router.get(
     "/profile",
@@ -17,5 +19,54 @@ router.get(
 
     }
 );
+
+
+router.get("/", async (req, res) => {
+    return res.status(200).json({
+        success: true, message: 'Testing is working withut user login !',
+    });
+})
+
+
+
+router.get("/states", async (req, res) => {
+    try {
+        const data = await State.find({
+            countryId: 101
+        }).sort({ craeteAt: -1 });
+
+        return res.status(200).json({
+            success: true, data: data
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false, message: errro.message
+        });
+    }
+});
+
+
+router.get("/cities-by-state/:id", async (req, res) => {
+
+    try {
+        const { id } = req.params;
+
+
+        const data = await City.find({
+            stateId: id
+        }).sort({ craeteAt: -1 });
+
+        return res.status(200).json({
+            success: true, data: data
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false, message: errro.message
+        });
+    }
+
+
+
+});
 
 module.exports = router;
